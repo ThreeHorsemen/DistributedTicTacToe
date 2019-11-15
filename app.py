052ce0dd738a9f1ba0308b722ced496d4e497515
Kim Toivonen <kim.toivonen@outlook.com>
2019-11-15 22:18:35 +0200

    Added reporting of winner to players and handling game reset

--- app.py
+++ app.py
@@ -7,19 +7,20 @@ from random import randrange
 app = Flask(__name__)
 
 state = {}
-number_of_players = 0
-game = [0 for i in range(9)]
+state['number_of_players'] = 0
+game = []
 
 id = 0
 
 @app.route('/game', methods = ['GET'])
 def foo():
+    global id
 
-    if number_of_players < 2:
+    if state['number_of_players'] < 2:
         id += 1
-        number_of_players += 1
+        state['number_of_players'] += 1
 
-        if number_of_players == 1:
+        if  state['number_of_players'] == 1:
             state['player1'] = id
         else:
             state['player2'] = id
@@ -32,6 +33,7 @@ def foo():
             else:
                 state['turn'] = state['player2']
 
+            game = [0 for i in range(9)]
 
         return jsonify(id = id, status = True)
     else:
@@ -41,27 +43,35 @@ def foo():
 @app.route('/heartbeat', methods = ['POST'])
 def heartbeat():
     data = json.loads(request.json)
-    id = data['id']
+    rid = data['id']
 
-    if state['turn'] == id:
-        return jsonify(value= True, winner = state['winner'], game=game)
+    if state['winner'] != 0:
+        state['players_informed_of_winner'] += 1
+
+        if state['players_informed_of_winner'] == 2:
+            reset_game()
+        return jsonify(value= True, game_ongoing= False, winner = state['winner'], game=game )
+
+    if state['turn'] == rid:
+        return jsonify(value= True, game=game, game_ongoing= True)
     else:
-        return jsonify(value=False)
+        return jsonify(value=False, game_ongoing= True)
 
 
 @app.route('/turn', methods = ['POST'])
 def process_turn():
     data = json.loads(request.json)
     move = data['index']
-    id = data['id']
+    rid = data['id']
 
     if game[move] != 0:
-        return 'hv'
+        return jsonify(value=False)
     else:
-        game[move] = id 
+        game[move] = rid 
 
         if check_if_game_has_been_won():
-            state['winner'] = id
+            state['winner'] = rid
+            state['players_informed_of_winner'] = 0
 
         
         if state['turn'] == state['player1']:
@@ -75,18 +85,28 @@ def process_turn():
 
 @app.route('/')
 def main_page():
-    print('Welcome')
+    return jsonify(value='TicTacToe')
+
+def reset_game():
+    state['number_of_players'] = 0
 
 
 def check_if_game_has_been_won():
-    if ((game[0] == game[1] == game[2]) and game[0] != 0) or
-    ((game[3] == game[4] == game[5]) and game[3] != 0) or
-    ((game[6] == game[7] == game[8]) and game[6] != 0) or
-    ((game[0] == game[3] == game[6]) and game[0] != 0) or
-    ((game[1] == game[4] == game[7]) and game[1] != 0) or
-    ((game[2] == game[5] == game[8]) and game[2] != 0) or
-    ((game[0] == game[4] == game[8]) and game[0] != 0) or
-    ((game[2] == game[4] == game[6]) and game[2] != 0):
+    if ((game[0] == game[1] == game[2]) and game[0] != 0):
+        return True
+    elif ((game[3] == game[4] == game[5]) and game[3] != 0):
+        return True
+    elif ((game[6] == game[7] == game[8]) and game[6] != 0):
+        return True
+    elif ((game[0] == game[3] == game[6]) and game[0] != 0):
+        return True
+    elif ((game[1] == game[4] == game[7]) and game[1] != 0):
+        return True
+    elif ((game[2] == game[5] == game[8]) and game[2] != 0):
+        return True
+    elif ((game[0] == game[4] == game[8]) and game[0] != 0):
+        return True
+    elif ((game[2] == game[4] == game[6]) and game[2] != 0):
         return True
     else:
         return False
