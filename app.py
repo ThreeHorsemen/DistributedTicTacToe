dff65172436cabdf30f8d06f74f1a1691748cf7e
Kim Toivonen <kim.toivonen@outlook.com>
2019-11-21 15:08:45 +0200

    draw check

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def heartbeat():
 
         if len(state['players_informed_of_winner']) == 2:
             reset_game()
-            
+
         return ret
 
     if state['turn'] == rid:
@@ -86,10 +86,13 @@ def process_turn():
     else:
         game[move] = rid 
 
-        if check_if_game_has_been_won():
+        result = check_if_game_has_been_won()
+        if  result == 1:
             state['winner'] = rid
             state['players_informed_of_winner'] = []
-
+        elif result == -1:
+            state['winner'] = -1
+            state['players_informed_of_winner'] = []
         
         if state['turn'] == state['player1']:
             state['turn'] = state['player2']
@@ -119,8 +122,14 @@ def check_if_game_has_been_won():
 
     for c in winning_combinations:
         if (game[c[0]] == game[c[1]] == game[c[2]]) and (game[c[0]] != 0):
-            return True
-    return False
+            return 1
+    
+    for i in game:
+        if i == 0:
+            return 0
+    
+    return -1
+    
 
 
 if __name__ == '__main__':
